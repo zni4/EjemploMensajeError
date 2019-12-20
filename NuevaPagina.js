d8765cacebc8cc24cbf0bbfea72abbe6da0faffc
zni4 <zni_4@hotmail.com>
2019-12-20 15:03:48 +0100

    Corrección errores

--- NuevaPagina.js
+++ NuevaPagina.js
@@ -14,34 +14,39 @@ function Mostrar(identidad, identidadBoton) {
 		document.getElementById(identidadBoton).textContent = "-";
 	}
 
-    //Los campos del arbol se definen div'#nivelppal'niv'#subnivel'-'#orden dentro del nivel'
-    var nivelPpal = Number(identidad.substring(identidad.indexOf("div") + 3, identidad.indexOf("niv")));
-    var subnivel = Number(identidad.substring(identidad.indexOf("niv") + 3, identidad.indexOf("-")));
-    var orden = Number(identidad.substring(identidad.indexOf("-") + 1, identidad.length));
+	//Los campos del arbol se definen div'#nivelppal'niv'#subnivel'-'#orden dentro del nivel'
+	var nivelPpal = Number(
+		identidad.substring(identidad.indexOf("div") + 3, identidad.indexOf("niv"))
+	);
+	var subnivel = Number(
+		identidad.substring(identidad.indexOf("niv") + 3, identidad.indexOf("-"))
+	);
+	var orden = Number(
+		identidad.substring(identidad.indexOf("-") + 1, identidad.length)
+	);
 
-	
-    for (let i = subnivel; i < 10; i++) {
-        for (let j = orden; j < 10; j++) {
-            identidad = "div" + nivelPpal + "niv" + i + "-" + j;
-            identidadBoton = "mas" + nivelPpal + "niv" + i + "-" + j;
+	for (let i = subnivel; i < 10; i++) {
+		for (let j = orden; j < 10; j++) {
+			identidad = "div" + nivelPpal + "niv" + i + "-" + j;
+			identidadBoton = "mas" + nivelPpal + "niv" + i + "-" + j;
 
-            if (document.getElementById(identidad)) {
-                if (
-                    document.getElementById(identidad).classList.contains("mostrar") &&
-                    mostrarOcultar.indexOf("-") != -1
-                ) {
-                    //contrae el texto si está expandido y lo expande en caso contrario
-                    document.getElementById(identidad).classList.remove("mostrar");
-                    document.getElementById(identidadBoton).textContent = "+";
-                } else {
-                    if (mostrarOcultar.indexOf("+") != -1) {
-                        document.getElementById(identidad).classList.add("mostrar");
-                        document.getElementById(identidadBoton).textContent = "-";
-                    }
-                }
-            }
-        }
-    }
+			if (document.getElementById(identidad)) {
+				if (
+					document.getElementById(identidad).classList.contains("mostrar") &&
+					mostrarOcultar.indexOf("-") != -1
+				) {
+					//contrae el texto si está expandido y lo expande en caso contrario
+					document.getElementById(identidad).classList.remove("mostrar");
+					document.getElementById(identidadBoton).textContent = "+";
+				} else {
+					if (mostrarOcultar.indexOf("+") != -1) {
+						document.getElementById(identidad).classList.add("mostrar");
+						document.getElementById(identidadBoton).textContent = "-";
+					}
+				}
+			}
+		}
+	}
 }
 
 function MostrarTodos() {
