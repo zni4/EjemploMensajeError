38a4fc05dff5d9e76bc49e01eeb77f7e35d08abf
zni4 <zni_4@hotmail.com>
2019-12-22 00:05:37 +0100

    Cambiamos el árbol guardando copia del anterior

--- NuevaPagina.js
+++ NuevaPagina.js
@@ -3,11 +3,6 @@ function Volver() {
 }
 
 function Mostrar(identidad, identidadBoton) {
-	var maximoSubnivel = 10;
-	var maximoOrden = 10;
-
-	var mostrarOcultar = document.getElementById(identidadBoton).textContent; //si vale + es que estoy mostrando, si vale - estoy ocultando
-
 	if (document.getElementById(identidad).classList.contains("mostrar")) {
 		//contrae el texto si está expandido y lo expande en caso contrario
 		document.getElementById(identidad).classList.remove("mostrar");
@@ -16,56 +11,28 @@ function Mostrar(identidad, identidadBoton) {
 		document.getElementById(identidad).classList.add("mostrar");
 		document.getElementById(identidadBoton).textContent = "-";
 	}
+}
 
-	//Los campos del arbol se definen div'#nivelppal'niv'#subnivel'-'#orden dentro del nivel'
-	var nivelPpal = Number(
-		identidad.substring(identidad.indexOf("div") + 3, identidad.indexOf("niv"))
-	);
-	var subnivel = Number(
-		identidad.substring(identidad.indexOf("niv") + 3, identidad.indexOf("-"))
-	);
-	var orden = Number(
-		identidad.substring(identidad.indexOf("-") + 1, identidad.length)
-	);
+var nivelMaximo = 10;
 
-	for (let i = subnivel; i < maximoSubnivel; i++) {
-		for (let j = orden; j < maximoOrden; j++) {
-			identidad = "div" + nivelPpal + "niv" + i + "-" + j;
-			identidadBoton = "mas" + nivelPpal + "niv" + i + "-" + j;
-
-			if (document.getElementById(identidad)) {
-				if (
-					document.getElementById(identidad).classList.contains("mostrar") &&
-					mostrarOcultar.indexOf("-") != -1
-				) {
-					//contrae el texto si está expandido y lo expande en caso contrario
-					document.getElementById(identidad).classList.remove("mostrar");
-					document.getElementById(identidadBoton).textContent = "+";
-				} else {
-					if (mostrarOcultar.indexOf("+") != -1) {
-						document.getElementById(identidad).classList.add("mostrar");
-						document.getElementById(identidadBoton).textContent = "-";
-					}
-				}
-			}
+function MostrarTodos() {
+	for (nivel = 1; nivel <= nivelMaximo; nivel++) {
+		var elmnt = document.getElementsByClassName("nivel" + nivel);
+		var i;
+		for (i = 0; i < elmnt.length; i++) {
+			elmnt[i].classList.add("mostrar");
+			elmnt[i].firstElementChild.textContent = "-";
 		}
 	}
 }
 
-function MostrarTodos() {
-	var elmnt = document.getElementsByClassName("contenedor");
-	var i;
-	for (i = 0; i < elmnt.length; i++) {
-		elmnt[i].classList.add("mostrar");
-		elmnt[i].firstElementChild.textContent = "-";
-	}
-}
-
 function OcultarTodos() {
-	var elmnt = document.getElementsByClassName("contenedor");
-	var i;
-	for (i = 0; i < elmnt.length; i++) {
-		elmnt[i].classList.remove("mostrar");
-		elmnt[i].firstElementChild.textContent = "+";
+	for (nivel = 1; nivel <= nivelMaximo; nivel++) {
+		var elmnt = document.getElementsByClassName("nivel" + nivel);
+		var i;
+		for (i = 0; i < elmnt.length; i++) {
+			elmnt[i].classList.remove("mostrar");
+			elmnt[i].firstElementChild.textContent = "+";
+		}
 	}
 }
