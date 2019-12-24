d752b33ee9702b8ac9d1cdd060a00ef069a2dac7
zni4 <zni_4@hotmail.com>
2019-12-24 07:36:42 +0100

    Incluyo Dropbox para expandir niveles

--- NuevaPagina.js
+++ NuevaPagina.js
@@ -3,43 +3,54 @@ function Volver() {
 }
 
 function definirBotones() {
-	MostrarTodos();
-	OcultarTodos();
+	MostrarTodos(10);
+	OcultarTodos(10);
 }
 
 function Mostrar(identidad, identidadBoton) {
-	if (document.getElementById(identidad).classList.contains("mostrar")) {
-		//contrae el texto si está expandido y lo expande en caso contrario
-		document.getElementById(identidad).classList.remove("mostrar");
-		document.getElementById(identidadBoton).textContent = "+";
-	} else {
-		document.getElementById(identidad).classList.add("mostrar");
-		document.getElementById(identidadBoton).textContent = "-";
+	if (document.getElementById(identidadBoton).textContent != "") {
+		if (document.getElementById(identidad).classList.contains("mostrar")) {
+			//contrae el texto si está expandido y lo expande en caso contrario
+			document.getElementById(identidad).classList.remove("mostrar");
+			document.getElementById(identidadBoton).textContent = "+";
+		} else {
+			document.getElementById(identidad).classList.add("mostrar");
+			document.getElementById(identidadBoton).textContent = "-";
+		}
 	}
 }
 
-var nivelMaximo = 10;
+function MostrarTodos(nivelMaximo) {
+	if (nivelMaximo == 10) {
+		document.getElementById("nivelSeleccionado").value = 10;
+	}
 
-function MostrarTodos() {
-	for (nivel = 1; nivel <= nivelMaximo; nivel++) {
-		var elmnt = document.getElementsByClassName("nivel" + nivel);
-		var i;
-		for (i = 0; i < elmnt.length; i++) {
-			elmnt[i].classList.add("mostrar");
+	if (nivelMaximo > 0) {
+		for (nivel = 1; nivel <= nivelMaximo; nivel++) {
+			var elmnt = document.getElementsByClassName("nivel" + nivel);
+			var i;
+			for (i = 0; i < elmnt.length; i++) {
+				elmnt[i].classList.add("mostrar");
 
-			if (
-				document.querySelector("#" + elmnt[i].id).children[2] //Existe un subnivel
-			) {
-				elmnt[i].firstElementChild.textContent = "-";
-			} else {
-				//No existe subnivel
-				elmnt[i].firstElementChild.textContent = "";
+				if (
+					document.querySelector("#" + elmnt[i].id).children[2] //Existe un subnivel
+				) {
+					elmnt[i].firstElementChild.textContent = "-";
+				} else {
+					//No existe subnivel
+					elmnt[i].firstElementChild.textContent = "";
+					elmnt[i].firstElementChild.style.backgroundColor = "white";
+				}
 			}
 		}
 	}
 }
 
-function OcultarTodos() {
+function OcultarTodos(nivelMaximo) {
+	// if (nivelMaximo == 10) {
+	// 	document.getElementById("nivelSeleccionado").value = 0;
+	// }
+
 	for (nivel = nivelMaximo; nivel >= 1; nivel--) {
 		var elmnt = document.getElementsByClassName("nivel" + nivel);
 		var i;
@@ -53,6 +64,7 @@ function OcultarTodos() {
 			} else {
 				//No existe subnivel
 				elmnt[i].firstElementChild.textContent = "";
+				elmnt[i].firstElementChild.style.backgroundColor = "white";
 			}
 		}
 	}
