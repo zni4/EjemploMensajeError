9e2808aa9b4d835e570c7e6d9a279c989d704fa0
zni4 <38941259+zni4@users.noreply.github.com>
2020-01-09 17:15:07 +0100

    Añado flecha en la Opción 3

--- Opcion3.js
+++ Opcion3.js
@@ -1,3 +1,55 @@
 function InicioOpcion3() {
 	document.getElementById("Menu3").classList.add("seleccionado");
 }
+
+function fijarquitarInfoSecundaria() {
+
+    if (document.getElementById("infoContenedor1").classList.contains("infoContenedor")) {
+        document.getElementById("infoContenedor1").classList.remove("infoContenedor");
+        document.getElementById("infoContenedor1").classList.add("infoContenedorMostrar");
+    }
+    else {
+        document.getElementById("infoContenedor1").classList.remove("infoContenedorMostrar");
+        document.getElementById("infoContenedor1").classList.add("infoContenedor");
+    }
+
+    if (document.getElementById("infoPrincipal1").classList.contains("infoPrincipal")) {
+        document.getElementById("infoPrincipal1").classList.remove("infoPrincipal");
+        document.getElementById("infoPrincipal1").classList.add("infoPrincipalMostrar");
+    }
+    else {
+        document.getElementById("infoPrincipal1").classList.remove("infoPrincipalMostrar");
+        document.getElementById("infoPrincipal1").classList.add("infoPrincipal");
+    }
+
+    if (document.getElementById("infoSecundaria1").classList.contains("infoSecundaria")) {
+        document.getElementById("infoSecundaria1").classList.remove("infoSecundaria");
+        document.getElementById("infoSecundaria1").classList.add("infoSecundariaMostrar");
+    }
+    else {
+        document.getElementById("infoSecundaria1").classList.remove("infoSecundariaMostrar");
+        document.getElementById("infoSecundaria1").classList.add("infoSecundaria");
+    }
+
+    if (document.getElementById("flechaDerecha1").classList.contains("flechaDerecha")) {
+        document.getElementById("flechaDerecha1").classList.remove("flechaDerecha");
+        document.getElementById("flechaDerecha1").classList.add("flechaDerechaMostrar");
+    }
+    else {
+        document.getElementById("flechaDerecha1").classList.remove("flechaDerechaMostrar");
+        document.getElementById("flechaDerecha1").classList.add("flechaDerecha");
+        document.getElementById("infoDerecha1").classList.remove("infoDerechaMostrar");
+        document.getElementById("infoDerecha1").classList.add("infoDerecha");
+    }
+}
+
+function mostrarDerecha() {
+    if (document.getElementById("infoDerecha1").classList.contains("infoDerecha")) {
+        document.getElementById("infoDerecha1").classList.remove("infoDerecha");
+        document.getElementById("infoDerecha1").classList.add("infoDerechaMostrar");
+    }
+    else {
+        document.getElementById("infoDerecha1").classList.remove("infoDerechaMostrar");
+        document.getElementById("infoDerecha1").classList.add("infoDerecha");
+    }
+}
